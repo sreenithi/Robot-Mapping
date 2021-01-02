8d4caf769fa9d93f2f18f17130f0d78ffe8633a1
Sreenithi Sridharan <19sreenithi98@gmail.com>
2021-01-02 16:04:17 +0400

    deleted additional script file

--- mapping.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-xterm -e " roslaunch my_robot world.launch" &
-sleep 5
-xterm -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py" &
-sleep 5
-xterm -e " roslaunch mapping mapping.launch"
