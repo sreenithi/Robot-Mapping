29c1d98445f45cdba05aa306ec2464c06df5952b
sreenithi <19sreenithi98@gmail.com>
2021-01-03 18:39:44 +0000

    updated world and 3d map

--- launch.sh
+++ launch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 xterm -e " roslaunch my_robot world.launch" &
-sleep 5
+sleep 10
+xterm -e " roslaunch mapping mapping.launch" &
+sleep 15
 xterm -e " rosrun teleop_twist_keyboard teleop_twist_keyboard.py" &
-sleep 5
-xterm -e " roslaunch mapping mapping.launch"
+
